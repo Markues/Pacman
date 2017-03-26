5885b365744de74d1d20299c376ca68ea33ef36e
Jeff Farris <jeff.markues@gmail.com>
2017-03-26 16:34:31 -0400

    more versatile texture class

--- PacMan/classes.h
+++ PacMan/classes.h
@@ -23,6 +23,9 @@ public:
 	bool loadFromRenderedText(string textureText, SDL_Color textColor);
 #endif
 	
+	// Creates blank texture
+	bool createBlank(int width, int height, SDL_TextureAccess = SDL_TEXTUREACCESS_STREAMING);
+	
 	// Deallocates texture
 	void free();
 	
@@ -33,18 +36,31 @@ public:
 	void setBlendMode(SDL_BlendMode blending);
 	
 	// Set alpha modulation
-	void setAlpha(Uint8 alpha);
+	void setAlpha( Uint8 alpha );
 	
 	// Renders texture at given point
 	void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	
+	// Set self as render target
+	void setAsRenderTarget();
 	
 	// Gets image dimensions
 	int getWidth();
 	int getHeight();
 	
+	// Pixel manipulators
+	bool lockTexture();
+	bool unlockTexture();
+	void* getPixels();
+	void copyPixels(void* pixels);
+	int getPitch();
+	Uint32 getPixel32(unsigned int x, unsigned int y);
+	
 private:
 	// The actual hardware texture
 	SDL_Texture* mTexture;
+	void* mPixels;
+	int mPitch;
 	
 	// Image dimensions
 	int mWidth;
